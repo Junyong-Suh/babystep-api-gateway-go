86f236f6c9e8e8283d9311c3ddfd3a49ce15541f
Junyong Suh <junyongsuh@gmail.com>
2019-05-08 10:05:12 +0900

    [BABY-7] Placeholder for echo handler unit test

--- handlers/echo_handler.go
+++ handlers/echo_handler.go
@@ -2,25 +2,10 @@ package handlers
 
 import (
     "net/http"
-    "io"
-    "io/ioutil"
 )
 
 func EchoHandler(w http.ResponseWriter, r *http.Request) {
-    // Stupid sinatra app that returns back info about the request
-    // https://github.com/3scale/echo-api
-    resp, err := http.Get("https://echo-api.3scale.net")
-    if err != nil {
-      // handle error
-    }
-    defer resp.Body.Close()
-
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-      // handle error
-    }
-
-    w.Header().Set("Content-Type", "application/json")
+    // Add Unit Tests
+    // https://github.com/Junyong-Suh/babystep-api-gateway-go/issues/16
     w.WriteHeader(resp.StatusCode)
-    io.WriteString(w, string(body))
 }
